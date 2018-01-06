f55f83e1ef8878a7e2d18f6b795ec9128b3fdf5a
trigoporres <santiagotrigoporres@gmail.com>
2018-01-06 20:54:02 +0100

    clase 1 instalacion y data binding

---
+++ instalacion-DataBinding/main.js
@@ -0,0 +1,15 @@
+// Vanilla Java script
+
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
+//
+// input.addEventListener('keyup', () => h1.innerHTML = input.value);
+
+//Vue js
+
+new Vue({
+  el: 'main',
+  data: {
+    mensaje: ''
+  }
+});
