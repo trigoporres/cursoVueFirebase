710724a8b6062af9559d2e0f9faccbec3eb71cac
trigoporres <santiagotrigoporres@gmail.com>
2018-01-26 15:31:00 +0100

    function render

---
+++ workflow/3.render-function/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <!-- de manera sencilla se puede añadir mas tag segun lo indiquemos en tipo -->
+        <elemento tipo="footer">Hola footer</elemento>
+        <elemento tipo="header">Hola header</elemento>
+        <elemento tipo="aside">Hola aside</elemento>
+        <elemento tipo="nav">Hola nav</elemento>
+        <elemento tipo="main">Hola main</elemento>
+        <elemento tipo="esteEsElTagQueSeRenderiza">Prueba</elemento>
+    </div>
+  
+</template>
+
+<script>
+  import elemento from './components/Elemento.vue';
+    export default {
+        components: { elemento }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ workflow/3.render-function/src/components/Elemento.vue
@@ -0,0 +1,37 @@
+<!--Esta seria la manera de hacerlo con template pero es largo repetitivo y poco mantenible, mejor hacerlo con la funcion render.
+  <template>
+    <div>
+        <article v-if="tipo === 'article'">
+            <slot></slot>
+        </article>
+        <section v-if="tipo === 'section'">
+            <slot></slot>
+        </section>
+        <aside v-if="tipo === 'aside'">
+            <slot></slot>
+        </aside>
+        <nav v-if="tipo === 'nav'">
+            <slot></slot>
+        </nav>
+        <header v-if="tipo === 'header'">
+            <slot></slot>
+        </header>
+        <footer v-if="tipo === 'footer'">
+            <slot></slot>
+        </footer>
+    </div>
+</template>-->
+<script>
+    export default {
+        props: ['tipo'],
+        render(createElement) {
+          //crea un elemento especificado por this.tipo y accedemos al texto con this.$slots.default
+            return createElement(
+                this.tipo,
+                this.$slots.default,
+            )
+        }
+    }
+</script>
+<style>
+</style>
