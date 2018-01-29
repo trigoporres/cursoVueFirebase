1d9aaa597bc94a6de92f5b035330c715e4d21ae5
trigoporres <santiagotrigoporres@gmail.com>
2018-01-29 14:20:50 +0100

    vue-state-tree

--- manejo de estados/2.vuexCentralState/src/components/Contador.vue
+++ manejo de estados/2.vuexCentralState/src/components/Contador.vue
@@ -6,6 +6,7 @@
     </div>
 </template>
 <script>
+//al utilizar vuex ya no necesitamos hacer referencia a ninguna propiedad y por lo tant ya no neceita emitir eventos ya que accedemos directamente al almacen principal donde tenemos declarada la propiedad compartida.
     export default {
         //props: ['cantidad'],
         methods: {
@@ -18,6 +19,7 @@
                 this.$store.state.cantidad--;
             }
         },
+        //de esta manera cuando hacemos referencia en template a cantidad que tenemos guardada en store y se lo devolvemos con el valor en ese momento
         computed: {
             cantidad() {
                 return this.$store.state.cantidad;
---
+++ manejo de estados/3. VuexStateTree/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hola me llamo {{ nombre }} {{ apellidos}} y me dedico a {{profesion}}</h1>
+</template>
+<script>
+    import {mapState} from 'vuex';
+    export default {
+/*        computed: {
+            nombre() {
+                return this.$store.state.nombre;
+            }
+        }*/
+/*        
+        mapState provee acceso a la instancia de vuex para poder crear computed de manera mas sencilla
+        computed: mapState({
+            nombre: (state) => state.nombre,
+            apellidos: (state) => state.apellidos,
+
+        })*/
+        computed: mapState(['nombre', 'apellidos', 'profesion']),
+    }
+</script>
